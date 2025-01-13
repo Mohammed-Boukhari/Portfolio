bd4549245a1296f2bac45149a058ef8da6cb70c3
mohamed <mohamed750boukhari@gmail.com>
2025-01-13 18:49:03 +0100

    i react || Creat Header UI Design

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Header from "./components/I-Header/Header";
+import Hero from "./components/II-Hero/Hero";
+import Main from "./components/III-Main/Main";
+import Contact from "./components/IV-Contact/Contact";
+import Footer from "./components/V-Footer/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      {/* Header component */}
+      <Header />
+      {/*== Header component ==*/}
+
+      <div className="divider" />
+
+      {/* Hero component */}
+      <Hero />
+      {/*== Hero component ==*/}
+
+      <div className="divider" />
+
+      {/* Main component */}
+      <Main />
+      {/*== Main component ==*/}
+
+      <div className="divider" />
+
+      {/* Contact component */}
+      <Contact />
+      {/*== Contact component ==*/}
+
+      <div className="divider" />
+
+      {/* Footer component */}
+      <Footer />
+      {/*== Footer component ==*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/I-Header/Header.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  const [showHaburgerMenu, setshowHaburgerMenu] = useState(false);
+
+  const closeMenu = () => {
+    setshowHaburgerMenu(false);
+  };
+  return (
+    <>
+      <header className="flex">
+        <button
+          onClick={() => {
+            setshowHaburgerMenu(!showHaburgerMenu);
+          }}
+          className="icon-menu hamburger-menu flex"
+        />
+
+        <div />
+        <nav>
+          <ul className="flex">
+            <li>
+              <a href="hero">About</a>
+            </li>
+            <li>
+              <a href="main">Articles</a>
+            </li>
+            <li>
+              <a href="contact">Projects</a>
+            </li>
+            <li>
+              <a href="contact">Speaking</a>
+            </li>
+            <li>
+              <a href="contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="mode flex">
+          <span className="icon-moon-o"></span>
+        </button>
+
+        {showHaburgerMenu && (
+          <div className="fixed ">
+            <ul className="modal">
+              <li className="border close ">
+                <button className="icon-cancel" onClick={closeMenu} />
+              </li>
+              <li style={{ paddingTop: "0" }}>
+                <a href="" onClick={closeMenu}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="" onClick={closeMenu}>
+                  Articles
+                </a>
+              </li>
+              <li>
+                <a href="" onClick={closeMenu}>
+                  Profects
+                </a>
+              </li>
+              <li>
+                <a href="" onClick={closeMenu}>
+                  Speaking
+                </a>
+              </li>
+              <li>
+                <a href="" onClick={closeMenu}>
+                  Uses
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </header>
+    </>
+  );
+};
+
+export default Header;
